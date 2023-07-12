0abd245586c96b1e0ed183cfb1d368d2f3b0167c
BilgeGates <xtaihuseynov131@gmail.com>
2023-07-12 15:45:19 +0400

    finished

--- my_react_app/src/components/footer/Footer.jsx
+++ my_react_app/src/components/footer/Footer.jsx
@@ -1,8 +1,60 @@
 import React from "react";
 import "./footer.css";
+import CTA from "./CTA";
+import CTATwo from "./CTATwo";
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div className="container footer__container">
+        <div className="footer__up">
+          <div className="list">
+            <div className="list__name">Company Info</div>
+            <ul>
+              <li>About Us</li>
+              <li>Carrier</li>
+              <li>We are hiring</li>
+              <li>Blog</li>
+            </ul>
+          </div>
+          <div className="list">
+            <div className="list__name">Legal</div>
+            <ul>
+              <li>About Us</li>
+              <li>Carrier</li>
+              <li>We are hiring</li>
+              <li>Blog</li>
+            </ul>
+          </div>
+          <div className="list">
+            <div className="list__name">Features</div>
+            <ul>
+              <li>Business Marketing</li>
+              <li>User Analytic</li>
+              <li>Live Chat</li>
+              <li>Unlimited Support</li>
+            </ul>
+          </div>
+          <div className="list">
+            <div className="list__name">Resources</div>
+            <ul>
+              <li>IOS & Android</li>
+              <li>Watch a Demo</li>
+              <li>Customers</li>
+              <li>API</li>
+            </ul>
+          </div>
+          <CTA />
+        </div>
+        <div className="footer__down">
+          <div className="col__1">
+            Made With Love By Figmaland All Right Reserved
+          </div>
+          <CTATwo />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- my_react_app/src/components/sectionseven/SectionSeven.jsx
+++ my_react_app/src/components/sectionseven/SectionSeven.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import "./sectionseven.css";
 
 const SectionSeven = () => {
-  return <section id="section7"></section>;
+  return (
+    <section id="section7">
+      <div className="container row__container">
+        <div className="info">
+          <h6>Newsletter</h6>
+          <h2>Our Experts Teacher</h2>
+          <p>
+            Problems trying to resolve the conflict between the two major realms
+            of Classical physics: Newtonian mechanics
+          </p>
+        </div>
+        <div className="contact">
+          <input type="email" placeholder="Your Email" />
+          <button>Subscribe</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SectionSeven;
